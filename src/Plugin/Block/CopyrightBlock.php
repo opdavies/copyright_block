c53c940b9a6dc92deb188a2f9f3e939078dc382e
bocaj <bocaj@582042.no-reply.drupal.org>
2016-09-30 23:26:05 +0100

    Issue #2665022 by bocaj: Port to Drupal 8

--- src/Plugin/Block/CopyrightBlock.php
+++ src/Plugin/Block/CopyrightBlock.php
@@ -8,6 +8,8 @@
 namespace Drupal\copyright_block\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Adds a copyright block.
@@ -17,15 +19,89 @@ use Drupal\Core\Block\BlockBase;
  *   admin_label = @Translation("Copyright block"),
  * )
  */
-class CopyrightBlock extends BlockBase {
+class CopyrightBlock extends BlockBase implements BlockPluginInterface {
+
+  public function settings() {
+    return $this->settings;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['start_year'] = array(
+      '#title' => t('Start year'),
+      '#type' => 'number',
+      '#min' => '1900',
+      '#max' => date('Y'),
+      '#required' => TRUE,
+      '#default_value' => $config['start_year'],
+      '#element_validate' => array($this, 'validateStartYear'),
+    );
+
+    $form['seperator'] = array(
+      '#title' => t('Seperator'),
+      '#type' => 'textfield',
+      '#required' => TRUE,
+      '#default_value' => $config['seperator'],
+    );
+
+    $form['text'] = array(
+      '#title' => t('Copyright statement text.'),
+      '#type' => 'text_format',
+      '#required' => TRUE,
+      '#default_value' => $config['text']['value'],
+      '#format' => $config['text']['format'],
+    );
+
+    $form['token_tree'] = array(
+      '#theme' => 'token_tree_link',
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfigurationValue('start_year', $form_state->getValue('start_year'));
+    $this->setConfigurationValue('seperator', $form_state->getValue('seperator'));
+    $this->setConfigurationValue('text', $form_state->getValue('text'));
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    return [
-      '#markup' => t('Copyright block'),
-    ];
+    $config = $this->getConfiguration();
+    $token = \Drupal::token();
+
+    // Token data.
+    $text = $token->replace($config['text']['value'], array(), array('config' => $config));
+
+    return array(
+      '#markup' => $text,
+    );
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $default_config = \Drupal::config('copyright_block.settings');
+
+    return array(
+      'start_year' => date('Y'),
+      'seperator' => $default_config->get('seperator'),
+      'text' => array(
+        'value' => $default_config->get('text.value'),
+        'format' => $default_config->get('text.format'),
+      ),
+    );
+  }
 }
